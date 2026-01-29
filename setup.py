1e0c8d749ed5351f9a339d33505ec81a61280678
Akash Navale <akashnavale04@gmail.com>
2026-01-29 16:37:46 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools  import find_packages,setup
+
+setup(
+    name="Medical-chatbot",
+    version="0.1.0",
+    author="Akash Navale",
+    author_email="akashnavale04@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    )
